7b373f8a27f6f019c1a3e32246921497736a99ff
alanflorendo <alan.florendo@gmail.com>
2014-07-25 15:27:26 -0400

    update migrations to reflect current schema

--- db/migrate/20140725114627_create_users_table.rb
+++ db/migrate/20140725114627_create_users_table.rb
@@ -2,7 +2,7 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
   	create_table :users do |t|
   		t.string :name
-  		t.string :password_hash
+  		t.string :password_digest
   		t.date :birthday
   		t.references :region
       t.string :email
--- db/migrate/20140725115502_create_surveys_table.rb
+++ db/migrate/20140725115502_create_surveys_table.rb
@@ -2,7 +2,7 @@ class CreateSurveysTable < ActiveRecord::Migration
   def change
   	create_table :surveys do |t|
   		t.string :name
-  		t.references :creator_user
+  		t.references :user
   		t.references :access_level
   		
   		t.timestamps
