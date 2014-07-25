d098563523960743cef1c98b1c5ccdff96c308e5
John Berry & Molly Huerster <pair+jpberry0623.mhuerster@devbootcamp.com>
2014-07-25 19:25:21 -0400

    add questions with ajax, add/rename survey routes

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,22 +1,24 @@
-get '/surveys/new' do 
-
+get '/surveys/new' do
 	erb :"surveys/new"
-end 
+end
 
-post '/surveys/create' do 
+post '/surveys/create' do
 	@survey = Survey.create(name: params[:name])
 
 	redirect "/surveys/#{@survey.id}"
 end
 
-get '/surveys/:id' do 
-	@survey = Survey.find(params[:id])	
-	erb :"surveys/show"
+get '/surveys/all' do
+	erb :"surveys/all_surveys"
+end
 
+get '/surveys/:id' do
+	@survey = Survey.find(params[:id])
+	erb :"surveys/show"
 end
 
 get '/surveys/:id/questions/new' do
-	@survey = Survey.find(params[:id])	
+	@survey = Survey.find(params[:id])
 	erb :"surveys/questions/new"
 end
 
@@ -24,15 +26,15 @@ end
 post "/surveys/:id/questions/create" do
 	@survey = Survey.find(params[:id])
 	@question = Question.create(prompt: params[:prompt], survey_id: @survey.id)
-	redirect "surveys/#{@survey.id}"
+	@question.to_json
+	# redirect "surveys/#{@survey.id}"
 end
 
 get "/surveys/:id/questions/:id/edit" do
 
-	@survey = nil 
+	@survey = nil
 
 end
 # get 'surveys/:id/edit' do
-# 	@survey = Survey.find(params[:id])	
+# 	@survey = Survey.find(params[:id])
 # end
-
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+   var surveyId = window.location.pathname.split('/')[2];
+  $("#new_question").submit(function(e){
+      e.preventDefault();
+      console.log(e);
+      console.log(surveyId);
+      $.ajax({
+        type: "POST",
+        data: $("#new_question").serialize(),
+        url: '/surveys/'+surveyId+'/questions/create',
+        dataType: "JSON",
+      }).success(function(data) {
+        console.log(data);
+        $("#survey_questions").append("<tr><td>"+data["id"]+"</td><td>"+data["prompt"]+"</td><td><a href=''>edit question</a></td></tr>");
+
+      }).fail(function(){
+
+      });
+
+
+      });
+    // );
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
