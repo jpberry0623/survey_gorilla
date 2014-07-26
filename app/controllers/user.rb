9425397b4888908f934b26cb0c409bfbd053170b
& <pair+alanflorendo.juljun14@devbootcamp.com>
2014-07-25 21:17:28 -0400

    add stats and surveys created (not taken) to user profile

---
+++ app/controllers/user.rb
@@ -0,0 +1,9 @@
+get '/users/:id' do
+  @user = User.find(params[:id])
+  @surveys = Survey.where(user_id: params[:id])
+  @results = Result.where(user_id: params[:id])
+  erb :'current_user/profile'
+end
+
+
+end
