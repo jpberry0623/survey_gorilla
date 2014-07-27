a271f3bb0c73fcc507b7d10370de56d346818e78
Rootul <rootulp@gmail.com>
2014-07-27 01:55:46 -0400

    extract chart logic into method in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,13 @@ class Question < ActiveRecord::Base
   has_one :question_type
   has_many :choices
   belongs_to :survey
+
+  def prepare_chart(results_hash)
+    data = []
+    self.choices.each do |choice|
+      data << [choice.label, results_hash[choice.id]]
+    end
+    return data
+  end
+
 end
