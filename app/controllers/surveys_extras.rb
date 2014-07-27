afef0cdf6bc7af18152794d23cfcf637bdaadc83
Rootul <rootulp@gmail.com>
2014-07-27 00:57:44 -0400

    after a user takes a survey, redirect to results of that survey

--- app/controllers/surveys_extras.rb
+++ app/controllers/surveys_extras.rb
@@ -2,7 +2,7 @@
 get '/surveys/:id/results' do
   @survey = Survey.find(params[:id])
   @results_hash = Result.group(:choice_id).count
-  erb :"/result/result_survey"
+  erb :"/surveys/extras/results"
 end
 
 # TAKE SURVEY
@@ -12,11 +12,11 @@ get '/surveys/:id/take' do
 end
 
 post '/surveys/:id/take' do
-  p "++++++++++++++++++++++++++++++++"
-  p session[:user_id]
-  p question = params[:question]
-  question.each_pair do |key, value|
+  # p "++++++++++++++++++++++++++++++++"
+  # p session[:user_id]
+  questions = params[:question]
+  questions.each_pair do |key, value|
     Result.create(user_id: session[:user_id], choice_id: value.to_i)
   end
-  redirect('/')
+  redirect('/surveys/' + params[:id] + '/results')
 end
