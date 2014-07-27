d75ec7f496817ba2f5608d7b9a7073c36ece8402
Rootul <rootulp@gmail.com>
2014-07-27 00:42:33 -0400

    attempt to clean up question / survey_controller routes

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,5 +1,4 @@
-# CREATE NEW SURVEY
-
+# NEW SURVEY
 get '/surveys/new' do
 	erb :"surveys/new"
 end
@@ -9,9 +8,10 @@ post '/surveys/create' do
 	redirect "/surveys/#{@survey.id}"
 end
 
-# EDIT A SURVEY
-# (its title and any other survey (not question/results) characteristics)
-# not needed for MVP
+# EDIT SURVEY
+# Should be able to change survey's title
+# and any other survey (non question/results) characteristics
+# Not needed for MVP
 get 'surveys/:id/edit' do
 end
 
@@ -19,7 +19,6 @@ patch '/surveys/:id' do
 end
 
 # DELETE SURVEY
-
 get '/surveys/:id/delete' do
 end
 
@@ -29,45 +28,16 @@ end
 
 
 
-# VIEW LIST OF SURVEYS
-
+# VIEW LIST OF ALL SURVEYS
 get '/surveys/all' do
 	erb :"surveys/all_surveys"
 end
 
 # VIEW INDIVIDUAL SURVEY (without results)
-
 get '/surveys/:id' do
 	@survey = Survey.find(params[:id])
 	erb :"surveys/show"
 end
-
-
-# WE SHOULD MOVE BELOW ROUTES TO SEPARATE FILES:  questions.rb
-#																							  	results.rb
-
-# CREATE NEW SURVEY QUESTION
-
-get '/surveys/:id/questions/new' do
-	@survey = Survey.find(params[:id])
-	erb :"surveys/questions/new"
-end
-
-post "/surveys/:id/questions/create" do
-	@survey = Survey.find(params[:id])
-	@question = Question.create(prompt: params[:prompt], survey_id: @survey.id)
-	@question.to_json
-	# redirect "surveys/#{@survey.id}"
-end
-
-# EDIT SURVEY QUESTION
-
-get "/surveys/:id/questions/:id/edit" do
-	@survey = nil
-end
-# get 'surveys/:id/edit' do
-# 	@survey = Survey.find(params[:id])
-# end
 
 # TAKE A SURVEY
 get '/surveys/:id/take' do
