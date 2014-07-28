e90df421eb30b11b4f59d4be73e5e49b053860df
Molly Huerster <mary.huerster@gmail.com>
2014-07-28 08:04:23 -0400

    move more code out

--- public/js/createSurvey.js
+++ public/js/createSurvey.js
@@ -20,29 +20,5 @@ $(document).ready(function() {
   });
 
 
-  $("#add_choice").click(function(e){
-    console.log(e);
-    var numChoices = $(".new_choice input[type='text']").length;
-    $(".new_choice").append("<input type='text' placeholder='new option' name='options[option"+(numChoices+1)+"]''><br>");
-  })
-  $(".new_choice").submit(function(e){
-    e.preventDefault();
-    $( ".question_choices" ).dialog( "close" );
-    $.ajax({
-      type: "POST",
-      data: $(".new_choice").serialize(),
-      url: $(".new_choice").attr("action"),
-      dataType: "JSON",
-    }).success(function(data) {
-      console.log(data.options);
-      $(".new_choice").each(function(){
-            this.reset();
-            return false;
-          });
-      var x;
-      for (option in data.options) {
-        $("#survey_in_progress ol li[id='"+data.question_id+"'] ul").append("<li>"+data.options[option]+"</li><br>");
-      };
-    });
-  })
+
 });
