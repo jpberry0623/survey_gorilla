dfe90759ec321c84ad3dfcd2ba5d7c4200eb3df9
Molly Huerster <mary.huerster@gmail.com>
2014-07-28 08:02:04 -0400

    move choice-creation code to separate file

---
+++ public/js/questionChoices.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  // Initialize question choices dialog
+  $(function() {
+    $(".question_choices").dialog({autoOpen : false, modal : true, show : "blind", hide : "blind"});
+  });
+})
