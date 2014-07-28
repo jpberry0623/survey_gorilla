b85c0f645e802fadc4d46c2210ae0b144e64fde0
Molly Huerster <mary.huerster@gmail.com>
2014-07-28 08:04:58 -0400

    add more choice-related code

--- public/js/questionChoices.js
+++ public/js/questionChoices.js
@@ -3,4 +3,33 @@ $(document).ready(function(){
   $(function() {
     $(".question_choices").dialog({autoOpen : false, modal : true, show : "blind", hide : "blind"});
   });
-})
+
+  // Create additional form fields based on user input
+  $("#add_choice").click(function(e){
+    console.log(e);
+    var numChoices = $(".new_choice input[type='text']").length;
+    $(".new_choice").append("<input type='text' placeholder='new option' name='options[option"+(numChoices+1)+"]''><br>");
+  })
+
+  // Submit newly created question options
+  $(".new_choice").submit(function(e){
+    e.preventDefault();
+    $( ".question_choices" ).dialog( "close" );
+    $.ajax({
+      type: "POST",
+      data: $(".new_choice").serialize(),
+      url: $(".new_choice").attr("action"),
+      dataType: "JSON",
+    }).success(function(data) {
+      console.log(data.options);
+      $(".new_choice").each(function(){
+            this.reset();
+            return false;
+          });
+      var x;
+      for (option in data.options) {
+        $("#survey_in_progress ol li[id='"+data.question_id+"'] ul").append("<li>"+data.options[option]+"</li><br>");
+      };
+    });
+  });
+});
